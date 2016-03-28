d40b6af6d2e1105a9b19d370bad529f6fa52a037
williamwc <williamhjcho@gmail.com>
2016-03-28 08:52:28 -0300

    PapelMoedaInvalidException implementado com o valor do PapelMoeda apresentado

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -1,9 +1,21 @@
 package br.calebe.ticketmachine.exception;
 
+import br.calebe.ticketmachine.core.PapelMoeda;
+
 /**
  *
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
+    
+    protected PapelMoeda papelMoeda;
+    
+    public PapelMoedaInvalidaException(PapelMoeda papelMoeda) {
+        this.papelMoeda = papelMoeda;
+    }
+
+    public PapelMoeda getPapelMoeda() {
+        return papelMoeda;
+    }
     
 }
